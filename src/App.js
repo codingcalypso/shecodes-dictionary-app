b0e2b298720347c3aafe0bc16b8a08af551bca1e
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-07-16 14:32:40 -0500

    Initial comments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img arc={logo} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
