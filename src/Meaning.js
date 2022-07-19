6ae7fbf63022c4ad5c39851eb2353a93c3999302
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-07-19 10:50:41 -0500

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,8 +10,7 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
-              {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
               <strong>Example:</strong>
               <em>{definition.example}</em>
